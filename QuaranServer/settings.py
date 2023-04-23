d36da1a8edd4c439b0bdbd25ba5673584646e96c
thanapatKJ <thanapatkjm@gmail.com>
2023-04-23 20:51:45 +0700

    new App password

--- QuaranServer/settings.py
+++ QuaranServer/settings.py
@@ -155,8 +155,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER='thanapat.qc@gmail.com'
-EMAIL_HOST_PASSWORD = 'tbwxgzltsttepkhl'
+EMAIL_HOST_USER='quaranclean.server@gmail.com'
+EMAIL_HOST_PASSWORD = 'erlekurlqkbwpply'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
